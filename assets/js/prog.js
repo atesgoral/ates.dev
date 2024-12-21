4334ac593adbede7908cb0322993432a7601281f
atesgoral <atesgoral@users.noreply.github.com>
2024-12-21 10:32:30 +0000

    deploy: f5de13f6766ebef7cb8230165382084dca22ee74

--- assets/js/prog.js
+++ assets/js/prog.js
@@ -203,10 +203,10 @@ function installServiceWorker() {
     navigator.serviceWorker
       .register('/service-worker.js')
       .then((registration) => {
-        console.log(
-          'Service Worker registered with scope:',
-          registration.scope,
-        );
+        // console.log(
+        //   'Service Worker registered with scope:',
+        //   registration.scope,
+        // );
       })
       .catch((error) => {
         console.error('Service Worker registration failed:', error);
